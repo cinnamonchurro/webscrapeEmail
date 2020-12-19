a6a163ad4af3a2c7af40f1e6696038424130f346
cinnamonchurro <lewyzisaac@gmail.com>
2020-12-20 03:25:04 +0800

    Changed to using BS4. Next will use regex

--- scaper.py
+++ scaper.py
@@ -1,5 +1,4 @@
-import logging
-import requests
+import logging, requests, bs4
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 logging.debug("Start of program")
 logging.disable(logging.CRITICAL)
@@ -7,11 +6,53 @@ logging.disable(logging.CRITICAL)
 website = "https://www.sfma.org.sg/member/members-directory"
 #website = "https://www.sfma.org.sg/member/info.php?cid=A000493"
 res = requests.get(website)
-#res.raise_for_status
-logging.debug(res.text)
-res.json()
-playFile = open("try.txt", "wb")
-loggin.debug(res)
-# for chunk in res.iter_content(100000) :
-#     playFile.write(chunk)
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+logging.debug(soup)
+#elems = soup.select("#myList > a:nth-child(1)")
+elems = soup.select("html body div.min-vh-100.container-fluid.bg-white.pb-3 div#myList.list-group a.list-group-item.list-group-item-action")
+
+playFile = open("scrape.txt", "w")
+logging.debug(elems)
+sum = 0
+for chunks in elems :
+     chunks = str(chunks)[56:76]
+     logging.info(chunks)
+     coySite = "https://www.sfma.org.sg/member/" + chunks
+     logging.info(coySite)
+     coyReq = requests.get(coySite)
+     coySoup = bs4.BeautifulSoup(coyReq.text, "html.parser")
+     logging.warning(coySoup)
+     coyElems = coySoup.select(".min-vh-100")
+     sum += 1
+     logging.warning(coyElems)
+     for i, coyChunks in enumerate(coyElems) :
+          logging.warning(coyChunks)
+          coyChunks = str(coyChunks)[213:485]
+          coyList = coyChunks.split("<strong>")
+          coyList = "".join(coyList)
+          coyList = coyList.split("</strong>")
+          coyList = "".join(coyList)
+
+
+
+          coyName = coyList.split("</h")[0]
+          # coyTel = coyList[9][9:]
+          # coyEmail = coyList[17][9:]
+          logging.debug(coyList)
+
+          # playFile.write(f"{coyName}, {coyTel}, {coyEmail} \n")
+
+
+          playFile.write(f"{coyName} \n")
+          # playFile.write(f"{coyTel} \n")
+          # playFile.write(f"{coyEmail} \n")
+
+     #      break
+     #
+     # break
+     # if(sum == 25) :
+     #      break
+
 
